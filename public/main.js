b954ef7474a6da0e374dc31b4e1407e12a2f230c
Yarnemeuris <yarne.meuris@gmail.com>
2025-11-12 22:25:05 +0100

    added parsing and calculating of the formula

--- public/main.js
+++ public/main.js
@@ -1,10 +1,51 @@
 document.addEventListener("keydown", (event) => {
     if (event.key !== "Enter") return;
-    console.log(document.querySelector("#formulaInput").value);
+
+    calculate(document.querySelector("#input").value)
 })
 
 function calculate(formula) {
+    if (formula == "") return "0.";
 
+    formula = formula.replaceAll(/\s/g, "");
+
+    if (formula.indexOf("(") != -1) {
+        var indecies = [formula.indexOf("("), formula.lastIndexOf(")")];
+        var parts = [formula.slice(0, indecies[0]), formula.slice(indecies[0] + 1, indecies[1]), formula.slice(indecies[1] + 1)];
+
+        parts[1] = calculate(parts[1])
+        formula = parts.join()
+    }
+
+    formula = formula.split(/([\+\-\*\/])/)
+
+    formula.forEach((value, index) => {
+        if (value.indexOf(".") != -1) return;
+        if (value.match(/[\+\-\*\/]/) != null) return;
+
+        formula[index] = value + "."
+    })
+
+    loop: {
+        while (true) {
+            for (var i = 1; ; i++) {
+                if (formula[i] == undefined) break loop;
+                if (formula[i].match(/[\+\-]/) == null) continue;
+                if (formula[i - 1] == undefined) continue;
+                if (formula[i + 1] == undefined) continue;
+
+                var num1 = formula[i] == "-" ? "-" + formula[i+1] : formula[i+1];
+                var num2 = formula[i-2] == "-" ? "-" + formula[i-1] : formula[i-1];
+
+                var result = add(num1, num2);
+                console.log(result, num1, num2);
+                formula.splice(i-1-(formula[i-2] == "-"), 3+(formula[i-2] == "-"), result);
+                break;
+            }
+        }
+    }
+
+    console.log(formula)
 }
 
 function digitsBeforePoint(number) {
@@ -21,7 +62,7 @@ function maxAbs(a, b) {
 
     if (digitsBeforePoint(a) != digitsBeforePoint(b)) return digitsBeforePoint(a) > digitsBeforePoint(b) ? a : b;
 
-    for (var i = 0; i < digitsBeforePoint(a); i++){
+    for (var i = 0; i < digitsBeforePoint(a); i++) {
         if (a[i] == b[i]) continue
 
         return a[i] > b[i] ? a : b
